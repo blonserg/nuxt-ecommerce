bbd585701d594b58cca249cd09944e6431c7fdb5
Tatarinov Valentin <tatarinov.valik@gmail.com>
2021-08-05 21:16:34 +0300

    test

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="container">
       <div class="row">
-          <div class="col-sm-2">
-
-          </div>
+        <div class="col-sm-2" style="color: red">test</div>
       </div>
     </div>
   </div>
