7697bad8c845a78732af290d5916b0ec2afbbb27
Tatarinov Valentin <tatarinov.valik@gmail.com>
2021-08-07 23:04:19 +0300

    footer

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,15 +1,72 @@
 <template>
-  <div сlass="footer" style="background-color: red">
+  <div class="footer">
     <div class="container">
-      <div class="row">
-        <div class="col-sm-2">
-          <p>Amino</p>
+      <div class="row footer-content">
+        <div class="col-sm-1">
+          <a class="footer-logo" href="#">
+            <p>Amino</p>
+          </a>
         </div>
 
-        <div class="col-sm-3 footer-shop"></div>
-        <div class="col-sm-3 footer-products"></div>
-        <div class="col-sm-4 footer-contacts"></div>
+        <div class="col-sm-3 footer-shop">
+          <span>Магазин</span>
+          <ul class="footer-shop">
+            <li><a href="#">Акции</a></li>
+            <li><a href="#">Оплата и доставка</a></li>
+            <li><a href="#">Статьи</a></li>
+            <br />
+            <li><a href="#">Контакты</a></li>
+            <li><a href="#">О нас</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-3 footer-products">
+          <span class="footer-products nav-products">Продукты</span>
+          <ul class="footer-products nav-group-1">
+            <li><a class="footer-products nav-item">Все категории</a></li>
+            <li><a class="footer-products nav-item">Спортивное питание</a></li>
+            <li><a class="footer-products nav-item">Аминокислоты</a></li>
+            <li><a class="footer-products nav-item">Аксессуары</a></li>
+            <li><a class="footer-products nav-item">Бренды</a></li>
+          </ul>
+        </div>
+
+        <div class="col-sm-5 footer-contacts">
+          <span class="footer-contacts text-contacts">Свяжитесь с нами</span>
+          <p class="footer-contacts text-contacts">
+            Принимаем звонки в будние дни с 8:00 до 23:00
+          </p>
+          <div class="footer-contacts info">
+            <img src="nuxt-ecommerce/assets/svg/phone.svg" alt="" />
+            <span class="footer-contacts info-text">+38 (098) 124-45-67</span>
+            <img src="nuxt-ecommerce/assets/svg/message.svg" alt="" />
+            <span class="footer-contacts info-text">amino@gmail.com</span>
+          </div>
+          <div class="footer-contacts form"></div>
+        </div>
+      </div>
+      <div class="row footer-social">
+        <div class="col-sm-6 footer-social info">
+          <img src="nuxt-ecommerce/assets/svg/visa.svg" alt="" />
+          <img src="nuxt-ecommerce/assets/svg/mastercard.svg" alt="" />
+        </div>
+        <div class="col-sm-6 footer-social info">
+          <img src="nuxt-ecommerce/assets/svg/telegram.svg" alt="" />
+          <img src="nuxt-ecommerce/assets/svg/instagram.svg" alt="" />
+        </div>
+      </div>
+      <div class="row footer-copyright">
+        <div class="col-sm-10 footer-copyright text">
+          <span>© Amino 2021. Все права защищены</span>
+          <a>Условия использования</a>
+          <a>Политика конфиденциальности</a>
+        </div>
+        <div class="col-sm-2 footer-copyright logo">
+          <img src="nuxt-ecommerce/assets/svg/logo-buro97.svg" alt="" />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" module>
+</style>
