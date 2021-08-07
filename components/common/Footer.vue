0a07fcf1f8007cb0d9fb3f2200d3982ac09d1563
Tatarinov Valentin <tatarinov.valik@gmail.com>
2021-08-07 11:17:30 +0300

    test-2

--- components/common/Footer.vue
+++ components/common/Footer.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
+  <div сlass="footer" style="background-color: red">
     <div class="container">
       <div class="row">
-        <div class="col-sm-2" style="color: red">test</div>
+        <div class="col-sm-2">
+          <p>Amino</p>
+        </div>
+
+        <div class="col-sm-3 footer-shop"></div>
+        <div class="col-sm-3 footer-products"></div>
+        <div class="col-sm-4 footer-contacts"></div>
       </div>
     </div>
   </div>
