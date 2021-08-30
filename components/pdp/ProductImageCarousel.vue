b41c6c62e5bd482bdf58be5f4c9640cd72201e4f
Sergei Barsukov <sergforweb@gmail.com>
2021-08-30 17:39:17 +0300

    fix carousel in pdp

--- components/pdp/ProductImageCarousel.vue
+++ components/pdp/ProductImageCarousel.vue
@@ -1,27 +1,34 @@
 <template>
-    <div class="product-image-carousel">
-        <div class="product-image-carousel_main">
-          <VueSlickCarousel
-            ref="imageCarousel"
-            :dots="true"
-            :focusOnSelect="true">
-            <div class="merch-main_image-item d-flex justify-content-center"
-                v-for="slide in imagesSet"
-                :key="slide.id">
-              <div class="product-images"><img src="@/assets/png/product-image.png" :alt="slide.title"></div>
-            </div>
-          </VueSlickCarousel>
+  <div class="product-image-carousel">
+    <div class="product-image-carousel_main">
+      <VueSlickCarousel
+        ref="imageCarousel"
+        :dots="true"
+        :focus-on-select="true"
+      >
+        <div
+          v-for="slide in imagesSet"
+          :key="slide.id"
+          class="merch-main_image-item d-flex justify-content-center"
+        >
+          <div class="product-images">
+            <img src="@/assets/png/product-image.png" :alt="slide.title" />
+          </div>
         </div>
-        <div class="product-image-carousel_nav image-carousel-nav">
-            <a href=""
-               class="image-carousel-nav_item active"
-               v-on:click.prevent="goToSlide"
-               v-for="item in imagesSet"
-               :key="item.id">
-                    <img src="@/assets/png/product-image.png" alt="">
-            </a>
-        </div>
+      </VueSlickCarousel>
     </div>
+    <div class="product-image-carousel_nav image-carousel-nav d-none d-md-block">
+      <a
+        v-for="item in imagesSet"
+        :key="item.id"
+        href=""
+        class="image-carousel-nav_item active"
+        @click.prevent="goToSlide"
+      >
+        <img src="@/assets/png/product-image.png" alt="" />
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,20 +37,20 @@ import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
-    components: {
-        VueSlickCarousel
-    },
-    props: {
-        imagesSet: {
-            type: Array,
-            required: true
-        }
-    },
-    methods: {
-        goToSlide: function(event) {
-            const imageIndex = [...event.currentTarget.parentElement.children].indexOf(event.currentTarget);
-            this.$refs.imageCarousel.goTo(imageIndex);
-        },
+  components: {
+    VueSlickCarousel
+  },
+  props: {
+    imagesSet: {
+      type: Array,
+      required: true
     }
+  },
+  methods: {
+    goToSlide: function (event) {
+      const imageIndex = Array.from(event.currentTarget.parentElement.children).indexOf(event.currentTarget)
+      this.$refs.imageCarousel.goTo(imageIndex)
+    }
+  }
 }
 </script>
