e8411c61a7b1f954a787b95364757fd4377f56c0
Oleksii Rohochyi <o.rohochyi@gmail.com>
2021-09-02 11:55:46 +0300

    Redesigned search box component in the header.

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -12,38 +12,13 @@
               :navigation-data="navData"
               :is-opened-nav="isOpenedNavigation"
             />
-            <SearchBox @onHideSearchBox="hideSearchBox" @onToggleSearchBox="toggleSearchBox" />
+            <SearchBox ref="searchBox" :isOpened="isOpenedSearchBox" @onHideSearchBox="hideSearchBox" @onToggleSearchBox="toggleSearchBox" />
             <div class="header-utils">
-              <button
-                v-b-toggle.sidebar-2
-                type="button"
-                class="d-lg-none search-block-toggler"
-                aria-expanded="false"
-              >
-                <svg
-                  width="14"
-                  height="15"
-                  viewBox="0 0 14 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M5.77686 12.1943C6.94141 12.1943 8.03271 11.8428 8.94092 11.2422L12.1489 14.4575C12.3613 14.6626 12.6323 14.7651 12.9253 14.7651C13.5332 14.7651 13.98 14.2891 13.98 13.6885C13.98 13.4102 13.8848 13.1392 13.6797 12.9341L10.4937 9.7334C11.1528 8.7959 11.541 7.66064 11.541 6.43018C11.541 3.25879 8.94824 0.666016 5.77686 0.666016C2.61279 0.666016 0.0126953 3.25879 0.0126953 6.43018C0.0126953 9.60156 2.60547 12.1943 5.77686 12.1943ZM5.77686 10.6562C3.45508 10.6562 1.55078 8.75195 1.55078 6.43018C1.55078 4.1084 3.45508 2.2041 5.77686 2.2041C8.09863 2.2041 10.0029 4.1084 10.0029 6.43018C10.0029 8.75195 8.09863 10.6562 5.77686 10.6562Z" fill="#E0ECFD" />
-                </svg>
-              </button>
               <CartButton />
               <b-button v-b-toggle.sidebar-1 class="d-md-none header-sidebar-btn">
                 <span></span>
               </b-button>
             </div>
-            <b-sidebar
-              id="sidebar-2"
-              shadow
-              @change="onToggleMobileNavigation"
-            >
-              <div class="header-sidebar">
-                <SearchBox />
-              </div>
-            </b-sidebar>
             <b-sidebar
               id="sidebar-1"
               shadow
@@ -133,9 +108,10 @@ export default {
     onToggleMobileNavigation (visible) {
       this.isOpenedNavigation = visible
     },
-    toggleSearchBox (e) {
+    toggleSearchBox(e) {
       if (this.isOpenedSearchBox && e.target.tagName === 'INPUT') { return }
-      this.isOpenedSearchBox = !this.isOpenedSearchBox
+      this.isOpenedSearchBox = !this.isOpenedSearchBox;
+      this.$refs.searchBox.$refs.typeaheadBootstrap.$refs.input.focus();
     },
     hideSearchBox () {
       this.isOpenedSearchBox = false
