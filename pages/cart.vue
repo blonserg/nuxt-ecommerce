f1791981510f05712bbdcc4b8927468c65fc302b
Sergei Barsukov <sergforweb@gmail.com>
2021-09-07 22:55:10 +0300

    fix count in cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,92 +1,104 @@
 <template>
   <div class="container">
     <client-only>
-         <div class="group">
-            <div class="group-left">
-                <div class="breadcrumbs">
-                    <ul>
-                        <li>
-                        <a href="/">Main</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="group-ttl">
-                    Корзина
-                </div>
-            </div>
+      <div class="group">
+        <div class="group-left">
+          <div class="breadcrumbs">
+            <ul>
+              <li>
+                <a href="/">Main</a>
+              </li>
+            </ul>
+          </div>
+          <div class="group-ttl">
+            Корзина
+          </div>
         </div>
-        <div v-if="getProductsInCart.length === 0" class="">
-            <p>
-                Товаров пока нет, но это легко можно исправить :)
-            </p>
-        </div>
-        <template v-else>
-            <div class="row">
-                <div class="col-md-8">
-                    <ProductsList class="products" :products-from-cart="getProducts" />
+      </div>
+      <div v-if="getProductsInCart.length === 0" class="">
+        <p>
+          Товаров пока нет, но это легко можно исправить :)
+        </p>
+      </div>
+      <template v-else>
+        <div class="row">
+          <div class="col-md-8">
+            <ProductsList class="products" :products-from-cart="getProducts" />
+          </div>
+          <div class="col-md-4">
+            <div class="cart-order">
+              <div class="cart-order-ttl">
+                Ваш заказ
+              </div>
+              <div class="cart-row">
+                <div class="cart-row-name">
+                  Всего товаров:
                 </div>
-                <div class="col-md-4">
-                    <div class="cart-order">
-                      <div class="cart-order-ttl">
-                          Ваш заказ
-                      </div>
-                      <div class="cart-row">
-                          <div class="cart-row-name">
-                              Всего товаров:
-                          </div>
-                          <div class="cart-row-info">
-                              6
-                              <span>ед</span>
-                          </div>
-                      </div>
-                      <div class="cart-row">
-                          <div class="cart-row-name">
-                              Скидка:
-                          </div>
-                          <div class="cart-row-info">
-                              0.00
-                              <span>₴</span>
-                          </div>
-                      </div>
-                      <div class="cart-row">
-                          <div class="cart-row-name">
-                              Сумма к оплате:
-                          </div>
-                          <div class="cart-row-info">
-                              1 800
-                              <span>₴</span>
-                          </div>
-                      </div>
-                      <div class="cart-promo">
-                          <label for="promo">Промокод</label>
-                          <div class="cart-promo-form">
-                            <b-form-input id="promo" placeholder="Введите промокод"></b-form-input>
-                            <b-button variant="primary" class="cart-promo-btn">
-                              <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1.75 1.5L6.25 6L1.75 10.5" stroke="#ffffff" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>
-                            </b-button>
-                          </div>
-                      </div>
-                      <div class="cart-total">
-                          <div class="cart-total-ttl">
-                              Итого:
-                          </div>
-                          <div class="cart-total-info">
-                              {{ getAmount | round }}
-                              <span class="cart-cur _total">₴</span>
-                          </div>
-                      </div>
-                      <nuxt-link
-                          to="/checkout"
-                          class="btn btn-primary cart-button"
-                          >
-                          Оформить заказ
-                      </nuxt-link>
-                    </div>
+                <div class="cart-row-info">
+                  {{ getQty }}
+                  <span>ед</span>
                 </div>
+              </div>
+              <div class="cart-row">
+                <div class="cart-row-name">
+                  Скидка:
+                </div>
+                <div class="cart-row-info">
+                  0.00
+                  <span>₴</span>
+                </div>
+              </div>
+              <div class="cart-row">
+                <div class="cart-row-name">
+                  Сумма к оплате:
+                </div>
+                <div class="cart-row-info">
+                  1 800
+                  <span>₴</span>
+                </div>
+              </div>
+              <div class="cart-promo">
+                <label for="promo">Промокод</label>
+                <div class="cart-promo-form">
+                  <b-form-input id="promo" placeholder="Введите промокод" />
+                  <b-button variant="primary" class="cart-promo-btn">
+                    <svg
+                      width="8"
+                      height="12"
+                      viewBox="0 0 8 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M1.75 1.5L6.25 6L1.75 10.5"
+                        stroke="#ffffff"
+                        stroke-width="1.6"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </b-button>
+                </div>
+              </div>
+              <div class="cart-total">
+                <div class="cart-total-ttl">
+                  Итого:
+                </div>
+                <div class="cart-total-info">
+                  {{ getAmount | round }}
+                  <span class="cart-cur _total">₴</span>
+                </div>
+              </div>
+              <nuxt-link
+                to="/checkout"
+                class="btn btn-primary cart-button"
+              >
+                Оформить заказ
+              </nuxt-link>
             </div>
-        </template>
+          </div>
+        </div>
+      </template>
     </client-only>
   </div>
 </template>
@@ -134,6 +146,18 @@ export default {
             parseInt(product.qty)
         })
         return amount
+      } else {
+        return 0
+      }
+    },
+    getQty () {
+      let qty = 0
+      if (this.getProductsInCart && this.getProductsInCart.length > 0) {
+        this.getProductsInCart.forEach(product => {
+          qty +=
+            parseInt(product.qty)
+        })
+        return qty
       } else {
         return 0
       }
