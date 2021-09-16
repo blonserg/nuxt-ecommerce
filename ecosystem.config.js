8b15ee7273c67e907c4c957aa50698f8b8d13847
Sergei Barsukov <sergforweb@gmail.com>
2021-09-16 13:19:07 +0300

    chng eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
       name      : 'MyAppName', // App name that shows in `pm2 ls`
       exec_mode : 'cluster', // enables clustering
       instances : 'max', // or an integer
-      cwd       : './current', // only if using a subdirectory
       script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
   }]
 };
