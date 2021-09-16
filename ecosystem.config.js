d89660a5562638e8aedbd49a77a84a6c4efc94de
Sergei Barsukov <sergforweb@gmail.com>
2021-09-16 13:34:21 +0300

    hotfix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
       name      : 'MyAppName', // App name that shows in `pm2 ls`
       exec_mode : 'cluster', // enables clustering
       instances : 'max', // or an integer
-      script    : './node_modules/nuxt/bin/nuxt-start', // The magic key
+      script    : "./node_modules/nuxt/bin/nuxt.js",
+      args      : "start",
   }]
 };
