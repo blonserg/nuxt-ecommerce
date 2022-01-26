cab6bac798267b2ca03dc6b740744684bb6b42f7
Valery Chebakov <arelov14@gmail.com>
2022-01-26 21:25:40 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@ export default {
   async asyncData ({ app, route, $axios, params, error, store }) {
     try {
       await store.dispatch('getCategoriesList')
-      const APPLIED_FILTERS_URL = 'https://aminostore.com.ua/api/products/?categories='
+      const APPLIED_FILTERS_URL = 'https://aminostore.com.ua/api/products/?ordering=price?categories='
       const protein = await $axios.$get(APPLIED_FILTERS_URL + '1')
       const aminokisloty = await $axios.$get(APPLIED_FILTERS_URL + '2')
       return { protein: protein.results, aminokisloty: aminokisloty.results }
