92c62d4522a5ad5730b73f82eba80e49485d826f
rockskeez <zhenya.vue@prof-it.bz>
2022-01-30 23:59:42 +0200

    fixed links

--- components/common/LinkMore.vue
+++ components/common/LinkMore.vue
@@ -5,8 +5,8 @@
   >
     {{ text }}
     <svg
-      width="8"
-      height="12"
+      width="6.5"
+      height="11"
       viewBox="0 0 8 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
