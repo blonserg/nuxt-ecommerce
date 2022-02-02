ffa2b963a8a6f5200d7fdaf95b0f441312ddcc6c
rockskeez <zhenya.vue@prof-it.bz>
2022-02-03 00:32:12 +0200

    code refactoring

--- components/common/CallBack.vue
+++ components/common/CallBack.vue
@@ -1,75 +1,80 @@
 <template>
-    <div :class="`${classes}`">
-        <b-form-input
-            placeholder="Номер телефона"
-            v-model="phone"
-            :state="isPhoneValid"
-            @input="validatePhone"
-            v-mask="'+38 (###) ###-##-##'">
-        </b-form-input>
-        <b-button
-            variant="primary"
-            class="btn-call-me"
-            @click="onClick">
-            Позвоните мне
-        </b-button>
-        <b-modal :ref="modalId" hide-footer title="Заявка успешно отправлена">
-            <div class="d-block">
-                <p>Ожидайте звонка от менеджера.</p>
-            </div>
-        </b-modal>
-    </div>
+  <div :class="`${classes}`">
+    <b-form-input
+      v-model="phone"
+      v-mask="'+38 (###) ###-##-##'"
+      placeholder="Номер телефона"
+      :state="isPhoneValid"
+      @input="validatePhone"
+    />
+    <b-button
+      variant="primary"
+      class="btn-call-me"
+      @click="onClick"
+    >
+      Позвоните мне
+    </b-button>
+    <b-modal
+      :ref="modalId"
+      hide-footer
+      title="Заявка успешно отправлена"
+    >
+      <div class="d-block">
+        <p>Ожидайте звонка от менеджера.</p>
+      </div>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 export default {
-    components: {
-    },
-    props: {
-        classes: {
-            type: String,
-        },
-    },
-  data() {
+  components: {
+  },
+  props: {
+    classes: {
+      type: String
+    }
+  },
+  data () {
     return {
-        phone: '',
-          isPhoneValid: null,
-        modalId: `modal-${this.$props.classes.split(' ')[0]}`, 
+      phone: '',
+      isPhoneValid: null,
+      modalId: `modal-${this.$props.classes.split(' ')[0]}`
     }
   },
   computed: {
   },
   methods: {
-    validatePhone() {
-        this.isPhoneValid = this.phone.length === 19 ? true : false
+    validatePhone () {
+      this.isPhoneValid = this.phone.length === 19
     },
-    onClick() {
-        if(!this.isPhoneValid) { return this.isPhoneValid = false; }    
+    onClick () {
+      if (!this.isPhoneValid) { return this.isPhoneValid = false }
       const payload = {
-        phone: `+${this.phone.replace(/\D+/g, "")}`
+        phone: `+${this.phone.replace(/\D+/g, '')}`
       }
-      this.isPhoneValid = null;
-      this.customerCallback(payload);
+      this.isPhoneValid = null
+      this.customerCallback(payload)
     },
-    showModal() {
-        this.$refs[this.modalId].show();
+    showModal () {
+      this.$refs[this.modalId].show()
     },
-    customerCallback(payload) {
-        const REQUEST_CALLBACK_URL = 'https://aminostore.com.ua/api/callback/';
+    customerCallback (payload) {
+      const REQUEST_CALLBACK_URL = 'https://aminostore.com.ua/api/callback/'
 
-        this.$axios({
+      this.$axios({
         method: 'POST',
         url: REQUEST_CALLBACK_URL,
         data: payload
-        })
+      })
         .then((response) => {
-            this.phone = '';
-            this.showModal();
+          this.phone = ''
+          this.showModal()
         })
         .catch((err) => {
-            throw new Error(err);
-        });
-    },
-  },
+          throw new Error(err)
+        })
+    }
+  }
 }
 </script>
