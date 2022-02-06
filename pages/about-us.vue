063be211d4640ec9e7426521c8bd1ce48b9f6f71
rockskeez <zhenya.vue@prof-it.bz>
2022-02-06 22:52:24 +0200

    commented bradcrumbs

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="container">
-      <div class="breadcrumbs">
+      <!-- <div class="breadcrumbs">
         <ul>
           <li>
             <a href="/">Main</a>
           </li>
         </ul>
-      </div>
+      </div> -->
       <div class="group">
         <div class="group-left">
           <div class="group-ttl">
